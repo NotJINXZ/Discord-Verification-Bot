f9a44d94c31f4428a15186d272bd60417df35744
JINXZ <58348317+NotJINXZ@users.noreply.github.com>
2023-05-19 20:48:53 -0400

    fix

--- databaseutil.py
+++ databaseutil.py
@@ -7,6 +7,7 @@ db = client["main"]
 collection = db["servers"]
 
 def get_data(server_id):
+    server_id = str(server_id)
     data = collection.find_one({"server_id": server_id})
     return data or {}
 
@@ -14,6 +15,7 @@ def save_data(data):
     collection.replace_one({"server_id": data["server_id"]}, data, upsert=True)
 
 def create_or_update_entry(server_id, staff_role_id="", verified_role_id="", premium=False, logging_webhook="", status=True):
+    server_id = str(server_id)
     existing_data = get_data(server_id)
     existing_data["server_id"] = server_id
     existing_data["staff_role_id"] = staff_role_id
@@ -24,44 +26,51 @@ def create_or_update_entry(server_id, staff_role_id="", verified_role_id="", pre
     save_data(existing_data)
 
 def get_data_for_server(server_id):
+    server_id = str(server_id)
     data = get_data(server_id)
     return data
 
 def delete_entry(server_id):
+    server_id = str(server_id)
     collection.delete_one({"server_id": server_id})
 
 def set_staff_role_id(server_id, staff_role_id):
+    server_id = str(server_id)
     data = get_data(server_id)
     data["staff_role_id"] = staff_role_id
     save_data(data)
 
 def set_verified_role_id(server_id, verified_role_id):
+    server_id = str(server_id)
     data = get_data(server_id)
     data["verified_role_id"] = verified_role_id
     save_data(data)
 
 def set_premium_status(server_id, premium_status):
+    server_id = str(server_id)
     data = get_data(server_id)
     data["premium"] = premium_status
     save_data(data)
 
 def set_logging_webhook(server_id, logging_webhook):
+    server_id = str(server_id)
     data = get_data(server_id)
     data["logging_webhook"] = logging_webhook
     save_data(data)
 
 def set_status(server_id, status):
+    server_id = str(server_id)
     data = get_data(server_id)
     data["status"] = status
     save_data(data)
 
 def get_logging_webhook_value(server_id):
+    server_id = str(server_id)
     data = get_data(server_id)
     return data.get("logging_webhook")
 
 def increment_total_verifications(server_id):
+    server_id = str(server_id)
     filter_query = {"server_id": server_id}
     update_query = {"$inc": {"total_verifications": 1}}
-
-    # Update the document, creating "total_verifications" field if it doesn't exist
     collection.update_one(filter_query, update_query, upsert=True)
